24ee81475f63d6cda5dd02b355ff0807fc6499c5
Brian <mscdex@gmail.com>
2010-04-29 10:42:38 -0700

    fixed typo in example

--- example.js
+++ example.js
@@ -5,7 +5,7 @@ conn.addListener('error', function(err) {
 	sys.puts('Uh oh, ' + err);
 });
 conn.addListener('row', function(rowdata, sql) {
-	sys.puts("'" + sql + "' generated a row: " + sys.inspect(row));
+	sys.puts("'" + sql + "' generated a row: " + sys.inspect(rowdata));
 });
 conn.addListener('queryDone', function(sql) {
 	sys.puts("Done with query: " + sql);
